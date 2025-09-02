682156b46dd42713fd6924f7cfb602b720f9b551
Sorin Deca <sorin.deca@yahoo.com>
2025-09-02 13:01:42 +0300

    first commit

---
+++ src/main/java/com/sd/store-management-api/StoreManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.sd.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementApiApplication.class, args);
+	}
+
+}
